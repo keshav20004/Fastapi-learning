a6c624667132300753b66cc4f1165763d5f160a0
Keshav Bajpai <keshavbajpai@Keshavs-MacBook-Air.local>
2026-02-08 19:05:04 +0530

    updated code

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ app=FastAPI()
 @app.get("/")
 def hell0():
     return {"message" :"Hello from fastapi testing git too"}
+
+@app.get("/about")
+def about():
+    return {"message":  "This is the info about keshav....he is an aiml student "}
+
